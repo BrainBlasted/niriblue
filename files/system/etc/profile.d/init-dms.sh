183549832dddfd70d82b04c215dc4bd38bb8b427
Christopher Davis <chris@brainblasted.dev>
2026-05-10 14:56:42 -0400

    Remove niri profile script for now

--- files/system/etc/profile.d/init-dms.sh
+++ files/system/etc/profile.d/init-dms.sh
@@ -1,7 +1,7 @@
 # shellcheck shell="sh"
-NIRI_CFG_DIR="$HOME/.config/"
-DEFAULT_CFG_DIR="/etc/dms/defaults/niri"
-
-if [ ! -f "$HOME/.config/niri/dms/" ]; then 
-   cp -r $DEFAULT_CFG_DIR "$NIRI_CFG_DIR"
-fi
+# NIRI_CFG_DIR="$HOME/.config/"
+# DEFAULT_CFG_DIR="/etc/dms/defaults/niri"
+# 
+# if [ ! -f "$HOME/.config/niri/dms/" ]; then 
+#    cp -r $DEFAULT_CFG_DIR "$NIRI_CFG_DIR"
+# fi
