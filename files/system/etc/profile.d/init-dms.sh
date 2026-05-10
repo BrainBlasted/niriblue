1e2e1dfee8032f29e6a4b49d2f4ea2cee9b7ebfc
Christopher Davis <chris@brainblasted.dev>
2026-05-10 17:43:09 -0400

    Don't exit non-interactive shells

--- files/system/etc/profile.d/init-dms.sh
+++ files/system/etc/profile.d/init-dms.sh
@@ -4,12 +4,10 @@ DEFAULT_CFG_DIR="/etc/dms/defaults/niri"
 # shellcheck disable=SC2034
 DMS_PRIVESC="sudo"
 
-if [[ ! $- == *i* ]]; then
-    exit
-fi
-
-if [[ ! -d "$HOME/.config/niri/dms/" ]]; then 
-   cp -r $DEFAULT_CFG_DIR "$NIRI_CFG_DIR"
-   DMS_PRIVESC="sudo" dms setup outputs
-   DMS_PRIVESC="sudo" dms setup cursor
+if [[ $- == *i* ]]; then
+    if [[ ! -d "$HOME/.config/niri/dms/" ]]; then 
+        cp -r $DEFAULT_CFG_DIR "$NIRI_CFG_DIR"
+        DMS_PRIVESC="sudo" dms setup outputs
+        DMS_PRIVESC="sudo" dms setup cursor
+    fi
 fi
