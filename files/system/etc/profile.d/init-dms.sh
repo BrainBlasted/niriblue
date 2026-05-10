180d541888abed63ce669ff93dc032b1f5ac7b16
Christopher Davis <chris@brainblasted.dev>
2026-05-10 17:25:56 -0400

    Attempt to filter out non-interactive shells & fix existence check

--- files/system/etc/profile.d/init-dms.sh
+++ files/system/etc/profile.d/init-dms.sh
@@ -1,10 +1,14 @@
-# shellcheck shell="sh"
+# shellcheck shell="bash"
 NIRI_CFG_DIR="$HOME/.config/"
 DEFAULT_CFG_DIR="/etc/dms/defaults/niri"
 # shellcheck disable=SC2034
 DMS_PRIVESC="sudo"
 
-if [ ! -f "$HOME/.config/niri/dms/" ]; then 
+if [[ ! $- == *i* ]]; then
+    exit
+fi
+
+if [[ ! -d "$HOME/.config/niri/dms/" ]]; then 
    cp -r $DEFAULT_CFG_DIR "$NIRI_CFG_DIR"
    DMS_PRIVESC="sudo" dms setup outputs
    DMS_PRIVESC="sudo" dms setup cursor
